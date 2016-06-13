c92df39cdd213fc128ec76ad41869ddf3374f695
shenzhouzd <shenzhouzhengdao@googlemail.com>
2016-06-13 17:01:08 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,48 @@
+var feed_data = {  
+    "domain":  { "name": "当前域名", 
+                 "value": [ "szzd.jamerson.org", "szzd.dora77.com", "szzd.icidweb.com"]
+               },
+ 
+    "article": { "name": "最新文章", 
+                 "value": [
+                           { "title": "世纪骗局 天安门自焚伪案揭秘", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hMTgVyjqwLzIwMTytMDUtMjyVMTExNi5odG1s" },
+                           { "title": "佛法修炼你知道多少？", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hMjIVyjI1LzIwMTytMDUtMTEVOTIwLmh0BWw" },
+                           { "title": "宇宙本身就是一个生命", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hMjIVyjI1LzIwMTytMDUtMTqVOTc5Lmh0BWw" }
+                          ]
+               },
+               
+     "video":  { "name": "精彩视频", 
+                 "value": [
+                           { "title": "是自焚还是骗局", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hNzqVyjMVMjaxNi0wNC0zMC83ODquAHRtBa" },
+                           { "title": "古今奇观-藏字石", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hNzqVyjMVMjaxNi0wNC0zMC83ODUuAHRtBa" },
+                           { "title": "【九评之一】评共产党是什么", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hMjgVyjMwLzIwMTytMDEtMTyVMTc5Lmh0BWw" }
+                          ]
+               },
+               
+    "software":{ "name": "翻墙软件",
+                 "value": [ 
+                            { "title": "自由门7.58版下载", "url": "/zym" },
+                            { "title": "无界浏览15.04下载", "url": "/wj" },
+                            { "title": "手机翻墙软件下载", "url": "/mobile" }
+                          ]
+               },
+               
+    "website": { "name": "网址导航",
+                 "value": [ 
+                            { "title": "法轮大法明慧网", "url": "/7/" },
+                            { "title": "动态网", "url": "/1/" },
+                            { "title": "新唐人电视台", "url": "/3/" }
+                          ]
+               },
+ 
+     "others": { "name": "",
+                 "value": [ 
+                            { "title": "", "url": "" },
+                            { "title": "", "url": "" },
+                            { "title": "", "url": "" }
+                          ]
+               },              
+               
+        "tui": { "url": "/tui/" }
+     
+} 
