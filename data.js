2e690c83fca92a221e7ac3086828f0894ee7d272
shenzhouzd <shenzhouzhengdao@googlemail.com>
2016-06-14 16:17:16 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,13 +1,14 @@
 var feed_data = {  
     "domain":  { "name": "当前域名", 
-                 "value": [ "szzd.jamerson.org", "szzd.dora77.com", "szzd.icidweb.com"]
+                 "value": [ "YzNwNlpDNXFZVzFsY25OdmJpNXZjbWM9", "YzNwNlpDNWtiM0poTnpjdVkyOXQ=", "YzNwNlpDNXBZMmxrZDJWaUxtTnZiUT09"]
                },
  
     "article": { "name": "最新文章", 
                  "value": [
                            { "title": "世纪骗局 天安门自焚伪案揭秘", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hMTgVyjqwLzIwMTytMDUtMjyVMTExNi5odG1s" },
                            { "title": "佛法修炼你知道多少？", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hMjIVyjI1LzIwMTytMDUtMTEVOTIwLmh0BWw" },
-                           { "title": "宇宙本身就是一个生命", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hMjIVyjI1LzIwMTytMDUtMTqVOTc5Lmh0BWw" }
+                           { "title": "宇宙本身就是一个生命", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hMjIVyjI1LzIwMTytMDUtMTqVOTc5Lmh0BWw" },
+                           { "title": "更多文章...", "url": "/" }
                           ]
                },
                
@@ -15,15 +16,17 @@ var feed_data = {
                  "value": [
                            { "title": "是自焚还是骗局", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hNzqVyjMVMjaxNi0wNC0zMC83ODquAHRtBa" },
                            { "title": "古今奇观-藏字石", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hNzqVyjMVMjaxNi0wNC0zMC83ODUuAHRtBa" },
-                           { "title": "【九评之一】评共产党是什么", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hMjgVyjMwLzIwMTytMDEtMTyVMTc5Lmh0BWw" }
+                           { "title": "【九评之一】评共产党是什么", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hMjgVyjMwLzIwMTytMDEtMTyVMTc5Lmh0BWw" },
+                           { "title": "更多文章...", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hNzqVyjMV" }
                           ]
                },
                
     "software":{ "name": "翻墙软件",
                  "value": [ 
-                            { "title": "自由门7.58版下载", "url": "/zym" },
+                            { "title": "电脑版翻墙软件", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9jB250yWN0Lmh0BWw" },
                             { "title": "无界浏览15.04下载", "url": "/wj" },
-                            { "title": "手机翻墙软件下载", "url": "/mobile" }
+                            { "title": "手机版翻墙软件", "url": "/mobile" },
+                            { "title": "更多软件...", "url": "/" }
                           ]
                },
                
@@ -31,22 +34,26 @@ var feed_data = {
                  "value": [ 
                             { "title": "法轮大法明慧网", "url": "/7/" },
                             { "title": "动态网", "url": "/1/" },
-                            { "title": "新唐人电视台", "url": "/3/" }
+                            { "title": "新唐人电视台", "url": "/3/" },
+                            { "title": "更多网址...", "url": "/" }
                           ]
                },
  
-     "others": { "name": "",
+     "others": { "name": "神州营救",
                  "value": [ 
-                            { "title": "", "url": "" },
-                            { "title": "", "url": "" },
-                            { "title": "", "url": "" }
+                            { "title": "紧急营救", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hMTgVyjIzLw" },
+                            { "title": "黑龙江营救", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hMTgVyjIzLw" },
+                            { "title": "山东营救", "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9hMTgVyjIzLw" },
+                            { "title": "更多内容...", "url": "/" }
                           ]
-               },
+               }, 
                
     "contact": { "name": "联系我们",
                  "url": "/?yvq=BG9jyWxoB3N0Ojk5OS9jB250yWN0Lmh0BWw"
-               },                
+               },               
                
-        "tui": { "url": "/tui/" }
+        "tui": { "url": "/999/",
+                 "value": "c2hlbnpob3V6aGVuZ2Rhby5vcmc="
+               }
      
-} 
+ }               
